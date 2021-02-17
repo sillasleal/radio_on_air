fed3c1bb26c04d46fb2b5ea6e51370f59344d389
Sillas S. Leal <sillas.s.leal@gmail.com>
2021-02-17 18:43:22 -0300

    exibida a mensagem de erro

--- pages/search/index.js
+++ pages/search/index.js
@@ -22,24 +22,28 @@ export default function Search({ podcasts }) {
     <div>
       <InputSearch />
       <h1>Resultado da busca:</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Podcast</th>
-            <th>Capa</th>
-          </tr>
-        </thead>
-        <tbody>
-          {podcasts.map((podcast, key) => (
-            <tr key={key}>
-              <td>{podcast.collectionName}</td>
-              <td>
-                <img src={podcast.artworkUrl30} />
-              </td>
+      {podcasts.length ? (
+        <table>
+          <thead>
+            <tr>
+              <th>Podcast</th>
+              <th>Capa</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {podcasts.map((podcast, key) => (
+              <tr key={key}>
+                <td>{podcast.collectionName}</td>
+                <td>
+                  <img src={podcast.artworkUrl30} />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <h4>Nenhum episódio encontrado</h4>
+      )}
     </div>
   );
 }
